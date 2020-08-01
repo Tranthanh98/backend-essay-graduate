de65983b71baf53b404ee552ef40d7bf0dff05cd
Trai <1620261@student.hcmus.edu.vn>
2020-08-01 10:14:23 +0700

    trai commit train face model

---
+++ RollCallSystem/Models/TrainFaceModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RollCallSystem.Models
+{
+    public class TrainFaceModel : TrainingImage
+    {
+        public string Base64Image { get; set; }
+        public HttpPostedFileBase Image { get; set; }
+    }
+}
